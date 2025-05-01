aad555e88a0943e0dfe2154713ef270c5d2842e5
Wahyu <wdprsto@gmail.com>
2025-05-01 15:59:48 +0700

    Update data not cached due to base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$proto = 'http';
+if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || 
+    (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+    $proto = 'https';
+}
+
+$config['base_url'] = $proto . '://' . $_SERVER['HTTP_HOST'] . '/';
 
 
 /*
