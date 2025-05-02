199fefc9b3264af31394e326e47f9d00ec5949e1
Wahyu <wdprsto@gmail.com>
2025-05-02 08:42:23 +0700

    Update header

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<title><?php echo $title; ?> | Humas PN Langsa</title>
+		<title><?php echo $title; ?></title>
 
 		<!-- Icon -->
 		<link rel="icon" href="<?php echo get_theme_uri('images/logo.png', 'made'); ?>">
